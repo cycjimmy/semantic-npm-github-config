2154b156b8d8a3e9a5d85c218d54481fd64c7828
cycjimmy <cycjimmy@gmail.com>
2020-05-27 17:06:57 +0800

    Revert "fix(*): fix git plugins"
    This reverts commit f9dd48643d02c85b34fcc2ad2d7ca59edd55d2c5.

--- release.config.js
+++ release.config.js
@@ -1,13 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const makeConfig = require('@cycjimmy/config-lib/semanticRelease/15.x/makeConfig');
+const makeCommonConfig = require('@cycjimmy/config-lib/semanticRelease/15.x/makeCommonConfig');
 
-module.exports = makeConfig({
-  npmOptions: {
-    npmPublish: false,
-  },
-  git: true,
-  gitAssets: [
-    "package.json",
-    "package-lock.json"
-  ]
-});
+module.exports = makeCommonConfig();
